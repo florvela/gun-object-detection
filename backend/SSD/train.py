055375c9dc12ff5cf716306438d68b4159961e0c
florvela <flor.p.vela@gmail.com>
2021-06-16 20:42:28 -0300

    train?

--- backend/SSD/train.py
+++ backend/SSD/train.py
@@ -28,7 +28,7 @@ args = {
     'training_split': './datasets/train/train_split_file.txt',
     'validation_split':  './datasets/valid/train_split_file.txt',
     'label_maps': ['0', '1', '2'],
-    'checkpoint': None, #'./output_2/cp_ep_100_loss_17.3806.h5', # can be an existing h5 to load weights from and continue training
+    'checkpoint': 'output_3/cp_ep_100_loss_7.3720.h5',#'/content/drive/MyDrive/ssd_train_output/cp_ep_30_loss_11.7435.h5', #'./output_2/cp_ep_100_loss_17.3806.h5', # can be an existing h5 to load weights from and continue training
     'checkpoint_type': 'epoch',
     'checkpoint_frequency': 100,
     'learning_rate': 0.0001,
@@ -36,7 +36,7 @@ args = {
     'batch_size': 64,
     'shuffle': True,
     'augment': False,
-    'output_dir': '/content/drive/MyDrive/ssd_train_output'
+    'output_dir': 'output_3'#'/content/drive/MyDrive/ssd_train_output'
 }
 
 # https://github.com/Socret360/object-detection-in-keras/tree/master/utils/ssd_utils
