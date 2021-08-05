882f2c42fb2a243abf63a3749eae224acd2fb9c6
florvela <flor.p.vela@gmail.com>
2021-08-05 12:41:31 -0300

    inference for yolo

--- src/yolo_utils/inference.py
+++ src/yolo_utils/inference.py
@@ -41,24 +41,3 @@ cv2.imshow("detections",frame)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# while cv2.waitKey(1) < 1:
-#     (grabbed, frame) = vc.read()
-#     if not grabbed:
-#         exit()
-#
-#     start = time.time()
-#     classes, scores, boxes = model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
-#     end = time.time()
-#
-#     start_drawing = time.time()
-#     for (classid, score, box) in zip(classes, scores, boxes):
-#         color = COLORS[int(classid) % len(COLORS)]
-#         label = "%s : %f" % (class_names[classid[0]], score)
-#         cv2.rectangle(frame, box, color, 2)
-#         cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-#     end_drawing = time.time()
-#
-#     fps_label = "FPS: %.2f (excluding drawing time of %.2fms)" % (
-#     1 / (end - start), (end_drawing - start_drawing) * 1000)
-#     cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-#     cv2.imshow("detections", frame)
