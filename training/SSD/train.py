3f3758edc8da715dddf140cd1618760ab0e4dda8
florvela <flor.p.vela@gmail.com>
2021-08-27 08:31:08 -0300

    train 3 batches

--- training/SSD/train.py
+++ training/SSD/train.py
@@ -28,16 +28,16 @@ args = {
     'training_split': './datasets/train/split.txt',
     'validation_split':  './datasets/valid/split.txt',
     'label_maps': ['0', '1', 'Shotgun'],
-    'checkpoint': '/content/drive/MyDrive/ssd_train_output_32_batches/cp_ep_170_loss_5.7802.h5', #'/content/drive/MyDrive/ssd_train_output/cp_ep_100_loss_7.3720.h5', #'/content/drive/MyDrive/ssd_train_output/cp_ep_30_loss_11.7435.h5', #'./output_2/cp_ep_100_loss_17.3806.h5', # can be an existing h5 to load weights from and continue training
+    'checkpoint': None, #'/content/drive/MyDrive/ssd_train_output_32_batches/cp_ep_170_loss_5.7802.h5', #'/content/drive/MyDrive/ssd_train_output/cp_ep_100_loss_7.3720.h5', #'/content/drive/MyDrive/ssd_train_output/cp_ep_30_loss_11.7435.h5', #'./output_2/cp_ep_100_loss_17.3806.h5', # can be an existing h5 to load weights from and continue training
     'checkpoint_type': 'epoch',
     'checkpoint_frequency': 10,
-    'initial_epoch': 170,
-    'learning_rate': 0.0001,
-    'epochs': 1000,
-    'batch_size': 32,
+    'initial_epoch': 0,
+    'learning_rate': 0.001,
+    'epochs': 100,
+    'batch_size': 3,
     'shuffle': True,
-    'augment': False,
-    'output_dir': '/content/drive/MyDrive/ssd_train_output_32_batches'
+    'augment': True,
+    'output_dir': '/content/drive/MyDrive/ssd_train_output_3_batches_augmented'
 }
 
 # https://github.com/Socret360/object-detection-in-keras/tree/master/utils/ssd_utils
