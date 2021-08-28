01188d0acbb3afef419ece45589cdb86b9e4f46a
florvela <flor.p.vela@gmail.com>
2021-08-28 16:21:34 -0300

    train model

--- training/SSD_2/train.sh
+++ training/SSD_2/train.sh
@@ -1,10 +1,10 @@
 python train.py \
 configs/ssd300_vgg16_pascal-voc-07-12.json \
-/content/gun-object-detection/training/SSD_2/dataset/output/images \
-/content/gun-object-detection/training/SSD_2/dataset/output/labels \
---training_split=/content/gun-object-detection/training/SSD_2/dataset/output/train.txt \
---validation_split=/content/gun-object-detection/training/SSD_2/dataset/output/val.txt \
---label_maps=/content/gun-object-detection/training/SSD_2/dataset/output/label_maps.txt \
+datasets/gunv3.v1i.voc/training_ready/images \
+datasets/gunv3.v1i.voc/training_ready/labels \
+--training_split=datasets/gunv3.v1i.voc/training_ready/train.txt \
+--validation_split=datasets/gunv3.v1i.voc/training_ready/val.txt \
+--label_maps=datasets/gunv3.v1i.voc/training_ready/label_maps.txt \
 --learning_rate=0.001 \
 --epochs=300 \
 --batch_size=3 \
