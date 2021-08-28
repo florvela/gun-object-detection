a996bfdc0861ddf7cc9a389a1a7103160c3b410b
florvela <flor.p.vela@gmail.com>
2021-08-28 12:00:06 -0300

    test training with pascal voc!

--- training/SSD_2/train.sh
+++ training/SSD_2/train.sh
@@ -1,13 +1,13 @@
 python train.py \
 configs/ssd300_vgg16_pascal-voc-07-12.json \
-/Users/socretlee/CodingDrive/other/object-detection-in-keras/data/pascal-voc-07-12/images \
-/Users/socretlee/CodingDrive/other/object-detection-in-keras/data/pascal-voc-07-12/labels \
---training_split=/Users/socretlee/CodingDrive/other/object-detection-in-keras/data/pascal-voc-07-12/train.txt \
---validation_split=/Users/socretlee/CodingDrive/other/object-detection-in-keras/data/pascal-voc-07-12/val.txt \
---label_maps=/Users/socretlee/CodingDrive/other/object-detection-in-keras/data/pascal-voc-07-12/label_maps.txt \
+/content/gun-object-detection/training/SSD_2/dataset/output/images \
+/content/gun-object-detection/training/SSD_2/dataset/output/labels \
+--training_split=/content/gun-object-detection/training/SSD_2/dataset/output/train.txt \
+--validation_split=/content/gun-object-detection/training/SSD_2/dataset/output/val.txt \
+--label_maps=/content/gun-object-detection/training/SSD_2/dataset/output/label_maps.txt \
 --learning_rate=0.001 \
 --epochs=2 \
 --batch_size=3 \
 --shuffle=True \
 --augment=True \
---output_dir=output/ssd300_vgg16_pascal-voc-2007
+--output_dir=/content/drive/MyDrive/testing_pascal
