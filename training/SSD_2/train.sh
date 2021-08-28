e0a06088d7bde71fab71d151303e17d0b9093fe0
florvela <flor.p.vela@gmail.com>
2021-08-28 16:37:22 -0300

    fix path

--- training/SSD_2/train.sh
+++ training/SSD_2/train.sh
@@ -1,10 +1,10 @@
 python train.py \
 configs/ssd300_vgg16_pascal-voc-07-12.json \
-datasets/gunv3.v1i.voc/training_ready/images \
-datasets/gunv3.v1i.voc/training_ready/labels \
---training_split=datasets/gunv3.v1i.voc/training_ready/train.txt \
---validation_split=datasets/gunv3.v1i.voc/training_ready/val.txt \
---label_maps=datasets/gunv3.v1i.voc/training_ready/label_maps.txt \
+datasets/training_ready/images \
+datasets/training_ready/labels \
+--training_split=datasets/training_ready/train.txt \
+--validation_split=datasets/training_ready/val.txt \
+--label_maps=datasets/training_ready/label_maps.txt \
 --learning_rate=0.001 \
 --epochs=300 \
 --batch_size=3 \
