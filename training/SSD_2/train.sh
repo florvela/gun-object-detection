bab2aeb523c09f3396279bda07a18d80463bb3f3
florvela <flor.p.vela@gmail.com>
2021-08-29 09:56:41 -0300

    train 1000 epochs

--- training/SSD_2/train.sh
+++ training/SSD_2/train.sh
@@ -6,8 +6,8 @@ datasets/training_ready/labels \
 --validation_split=datasets/training_ready/val.txt \
 --label_maps=datasets/training_ready/label_maps.txt \
 --learning_rate=0.001 \
---epochs=100 \
+--epochs=1000 \
 --batch_size=3 \
 --shuffle=True \
 --augment=True \
---output_dir=output
+--output_dir=output/1000_epochs
